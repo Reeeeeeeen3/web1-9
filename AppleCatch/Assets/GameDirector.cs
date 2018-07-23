8d4723064c38471c481ffbd9dc219ca577871985
Unknown <g1727071@st.t-kougei.ac.jp>
2018-07-23 18:21:02 +0900

    ちょっとした変更

--- AppleCatch/Assets/GameDirector.cs
+++ AppleCatch/Assets/GameDirector.cs
@@ -7,8 +7,10 @@ public class GameDirector : MonoBehaviour {
 
     GameObject timerText;
     GameObject pointText;
-    float time = 60.0f;
+    float time = 30.0f;
     int point = 0;
+    GameObject generator;
+    GameObject CongratulationText;
 
     public void GetApple()
     {
@@ -22,16 +24,60 @@ public class GameDirector : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        this.generator = GameObject.Find("ItemGenerator");
         this.timerText = GameObject.Find("Time");
         this.pointText = GameObject.Find("Point");
+        this.CongratulationText = GameObject.Find("Congratulation");
     }
 	
 	// Update is called once per frame
 	void Update () {
         this.time -= Time.deltaTime;
+
+        if(this.time < 0)
+        {
+            if (this.point <= 1000)
+            {
+                this.CongratulationText.GetComponent<Text>().text = "BAD";
+            }
+            else if (1000 <= this.point && this.point < 1500)
+            {
+                this.CongratulationText.GetComponent<Text>().text = "GOOD";
+            }
+            else if (1500 <= this.point && this.point < 2000)
+            {
+                this.CongratulationText.GetComponent<Text>().text = "Great!";
+            }
+            else if (2000 <= this.point)
+            {
+                this.CongratulationText.GetComponent<Text>().text = "Fantastic!!";
+            }
+
+            this.time = 0;
+            this.generator.GetComponent<ItemGenerator>().SetParameter(100000.0f, 0, 0);
+        }else if(0 <= this.time && this.time < 5)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(
+                0.9f, -0.04f, 3);
+        }else if(5 <= this.time && this.time < 10)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(
+                0.4f, -0.06f, 6);
+        }
+        else if (10 <= this.time && this.time < 20)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(
+                0.7f, -0.04f, 4);
+        }
+        else if (20 <= this.time && this.time < 30)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(
+                1.0f, -0.03f, 2);
+        }
         this.timerText.GetComponent<Text>().text =
             this.time.ToString("F1");
         this.pointText.GetComponent<Text>().text =
             this.point.ToString() + " point";
-	}
+
+    }
 }
